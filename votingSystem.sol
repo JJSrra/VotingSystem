7ec5b5aedba2eea760fea0f34e7951a6584b15b2
Juanjo Sierra <jjsierra103@gmail.com>
2018-03-07 16:56:50 +0100

    Added owner figure, parties are now called candidates

--- votingSystem.sol
+++ votingSystem.sol
@@ -5,38 +5,44 @@ contract VotingSystem{
 
     // Voter struct, representing each voter, where:
     // - alreadyVoted is a flag that is active if this voter has already voted
-    // - vote is the index of this voter's vote
+    // - vote is the index of this voter's voted candidate
     struct Voter{
         bool alreadyVoted;
         uint vote;
     }
 
-    // Party struct, representing each party, where:
-    // - name represents each party's acronym
-    // - votes is the count of the votes each party has received
-    struct Party{
+    // Candidate struct, representing each vote option, where:
+    // - name represents each candidate's name
+    // - votes is the count of the votes each candidate has received
+    struct Candidate{
         bytes32 name;
         uint votes;
     }
 
-    // Variables that will store all voters and parties
+    // Variables that will store all voters and candidates
     mapping (address => Voter) public voters;
-    Party[] public parties;
+    Candidate[] public candidates;
+
+    // Address that identifies the owner of the voting
+    address public owner;
 
     // Constructor function, that receives an array with the name
-    // of each party, and creates every necessary slot in "parties" array
-    function VotingSystem (bytes32[] partiesNames) public {
-        for (uint i=0; i < partiesNames.length; ++i){
-            // The party is included in the array
-            parties.push(Party({
-                name: partiesNames[i],
+    // of each candidate, and creates every necessary slot in "candidates" array
+    // NOTE: The user that creates the voting becomes the owner of the voting,
+    // thus, the only one able to close it
+    function VotingSystem (bytes32[] candidatesNames) public {
+        owner = msg.sender;
+        for (uint i=0; i < candidatesNames.length; ++i){
+            // The candidate is included in the array
+            candidates.push(Candidate({
+                name: candidatesNames[i],
                 votes: 0
             }));
         }
     }
 
     // Voting function, can only be called once per voter
-    function Vote (uint partyVote) public {
+    function Vote (uint candidateVote) public {
         // First the currentVoter has to be located, and it is mandatory
         // to check if they have already voted
         Voter storage currentVoter = voters[msg.sender];
@@ -44,11 +50,11 @@ contract VotingSystem{
 
         // If they have not already voted, the flag is switched to true,
         // and then the vote is registered
-        currentVoter.vote = partyVote;
+        currentVoter.vote = candidateVote;
         currentVoter.alreadyVoted = true;
 
-        // And of course, the vote is added to the party's count
-        parties[partyVote].votes++;
+        // And of course, the vote is added to the candidate's count
+        candidates[candidateVote].votes++;
 
     }
 }
